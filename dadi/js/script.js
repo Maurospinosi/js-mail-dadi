37f1509d152baac48df2a2b9f7e13e55f2299f2a
Mauro <mauro.spinosi94@gmail.com>
2020-08-24 18:11:44 +0200

    Eser. Dadi

---
+++ dadi/js/script.js
@@ -0,0 +1,16 @@
+var utente = Math.floor(Math.random() * 6) + 1;
+var pc = Math.floor(Math.random() * 6) + 1;
+document.getElementById('dado').innerHTML ="Utente :" + utente;
+document.getElementById('dado_pc').innerHTML ="PC :" + pc;
+
+
+  if (utente > pc) {
+    utente = "Win";
+    document.getElementById('result').innerHTML = utente;
+  }else if (utente == pc) {
+    utente = "Draw";
+    document.getElementById('result').innerHTML = utente;
+  } else{
+    utente = "Lose";
+    document.getElementById('result').innerHTML = utente;
+  }
