7d95270fd6744edf0367fda386d2585c3a410bed
Mauro <mauro.spinosi94@gmail.com>
2020-08-24 18:24:16 +0200

    Bonus (un po di stile)

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,7 +1,7 @@
 var utente = Math.floor(Math.random() * 6) + 1;
 var pc = Math.floor(Math.random() * 6) + 1;
-document.getElementById('dado').innerHTML ="Utente :" + utente;
-document.getElementById('dado_pc').innerHTML ="PC :" + pc;
+document.getElementById('dado').innerHTML = utente;
+document.getElementById('dado_pc').innerHTML = pc;
 
 
   if (utente > pc) {
